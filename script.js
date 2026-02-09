630c43c32258c80b56bb2753dbc1742989bc23ff
Aryan Sirohi <aryansirohi28@gmail.com>
2026-02-09 11:03:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,55 @@
-// Elements
-const envelope = document.getElementById("envelope-container");
-const letter = document.getElementById("letter-container");
-const noBtn = document.querySelector(".no-btn");
-const yesBtn = document.querySelector(".btn[alt='Yes']");
+window.addEventListener("DOMContentLoaded", () => {
 
-const title = document.getElementById("letter-title");
-const catImg = document.getElementById("letter-cat");
-const buttons = document.getElementById("letter-buttons");
-const finalText = document.getElementById("final-text");
+    // Elements
+    const envelope = document.getElementById("envelope-container");
+    const letter = document.getElementById("letter-container");
+    const noBtn = document.querySelector(".no-btn");
+    const yesBtn = document.querySelector(".btn[alt='Yes']");
 
-// Click Envelope
+    const title = document.getElementById("letter-title");
+    const catImg = document.getElementById("letter-cat");
+    const buttons = document.getElementById("letter-buttons");
+    const finalText = document.getElementById("final-text");
 
-envelope.addEventListener("click", () => {
-    envelope.style.display = "none";
-    letter.style.display = "flex";
+    const music = document.getElementById("bg-music");
 
-    setTimeout( () => {
-        document.querySelector(".letter-window").classList.add("open");
-    },50);
-});
+    // Click Envelope
+    envelope.addEventListener("click", () => {
+        envelope.style.display = "none";
+        letter.style.display = "flex";
 
-// Logic to move the NO btn
+        // play music when envelope opens
+        music.play().catch((err) => {
+            console.log("Audio play failed:", err);
+        });
 
-noBtn.addEventListener("mouseover", () => {
-    const min = 200;
-    const max = 200;
+        setTimeout(() => {
+            document.querySelector(".letter-window").classList.add("open");
+        }, 50);
+    });
 
-    const distance = Math.random() * (max - min) + min;
-    const angle = Math.random() * Math.PI * 2;
+    // Logic to move the NO btn
+    noBtn.addEventListener("mouseover", () => {
+        const min = 200;
+        const max = 200;
 
-    const moveX = Math.cos(angle) * distance;
-    const moveY = Math.sin(angle) * distance;
+        const distance = Math.random() * (max - min) + min;
+        const angle = Math.random() * Math.PI * 2;
 
-    noBtn.style.transition = "transform 0.3s ease";
-    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
-});
+        const moveX = Math.cos(angle) * distance;
+        const moveY = Math.sin(angle) * distance;
 
-// Logic to make YES btn to grow
+        noBtn.style.transition = "transform 0.3s ease";
+        noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    });
 
-// let yesScale = 1;
+    // YES is clicked
+    yesBtn.addEventListener("click", () => {
+        title.textContent = "Yippeeee!";
+        catImg.src = "cat_dance.gif";
+        document.querySelector(".letter-window").classList.add("final");
+        buttons.style.display = "none";
+        finalText.style.display = "block";
+    });
 
-// yesBtn.style.position = "relative"
-// yesBtn.style.transformOrigin = "center center";
-// yesBtn.style.transition = "transform 0.3s ease";
-
-// noBtn.addEventListener("click", () => {
-//     yesScale += 2;
-
-//     if (yesBtn.style.position !== "fixed") {
-//         yesBtn.style.position = "fixed";
-//         yesBtn.style.top = "50%";
-//         yesBtn.style.left = "50%";
-//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-//     }else{
-//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-//     }
-// });
-
-// YES is clicked
-
-yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
-
-    catImg.src = "cat_dance.gif";
-
-    document.querySelector(".letter-window").classList.add("final");
-
-    buttons.style.display = "none";
-
-    finalText.style.display = "block";
-});
-const envelope = document.getElementById("envelope");
-const music = document.getElementById("bg-music");
-
-envelope.addEventListener("click", () => {
-  music.play().catch((err) => {
-    console.log("Audio play failed:", err);
-  });
 });
